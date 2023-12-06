c72e3b5b6b894eae2b3a795567b4d01878f22d28
AjayKumar Wade <92783730+AjayKumar049@users.noreply.github.com>
2023-12-06 09:41:03 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Login from './Components/login';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Built a login using ReactJs with MaterialUI</h1>
+      <Login />
+      </div>
+  );
+}
+
+export default App;
